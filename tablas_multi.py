f0e937045f6dd4d40e54d5d61a1a85d8701808ba
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-04-11 20:08:09 +0200

    Create tablas_multi.py

---
+++ tablas_multi.py
@@ -0,0 +1,17 @@
+from VALID import OK, ns
+import subprocess
+
+while True:
+    print("TABLAS DE MULTIPLICAR")
+    numero=OK(input("Dame el número: "))
+
+    for i in range(0,11):
+        resul=i*numero
+        print(("%d x %d = %d")%(numero,i,resul))
+
+    conti=ns(input("¿Desea continuar?: "))
+    if conti==("n"):
+        break
+    else:
+        subprocess.call(["cmd.exe","/C","cls"])
+
